48442c8f44de177d56c4c790988179d708fd84db
temautc <tema.utc@gmail.com>
2023-11-13 23:43:03 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import * as React from 'react';
+import Home from './components/home/home';
+import Header from './components/common/header';
+import Footer from './components/common/footer';
+
+import './index.css'
+function App() {
+  return (
+    <>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/home/home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className='text-red-200'>home</div>
+  )
+}
+
+export default home
